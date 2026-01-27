6b9c6396f7ea2f6c50580eccfe3e79394d00da14
Stuart Clark <stuart.clark@Jahingo.com>
2026-01-27 12:55:55 +0000

    feat: add terraform bootstrap config for s3 backend

---
+++ terraform/bootstrap.tf
@@ -0,0 +1,112 @@
+data "aws_caller_identity" "current" {}
+
+data "external" "git_details" {
+  program = ["${path.module}/scripts/get_repo_details.sh"]
+}
+
+locals {
+  account_id  = data.aws_caller_identity.current.account_id
+  repo_name   = data.external.git_details.result.name
+  repo_url    = data.external.git_details.result.url
+  bucket_name = "${local.account_id}-${local.repo_name}-state"
+
+  common_tags = {
+    Repository = local.repo_url
+    Type       = "Terraform"
+  }
+}
+
+module "s3_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = ">= 3.0"
+
+  bucket = local.bucket_name
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = true
+  }
+
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+  tags = local.common_tags
+}
+
+module "dynamodb_table" {
+  source  = "terraform-aws-modules/dynamodb-table/aws"
+  version = ">= 3.0"
+
+  name     = "${local.bucket_name}-lock"
+  hash_key = "LockID"
+
+  attributes = [
+    {
+      name = "LockID"
+      type = "S"
+    }
+  ]
+
+  tags = local.common_tags
+}
+
+module "iam_user" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-user"
+  version = ">= 5.0"
+
+  name = "${local.repo_name}-terraform-state-user"
+
+  create_iam_access_key         = true
+  create_iam_user_login_profile = false
+
+  force_destroy = true
+
+  tags = local.common_tags
+}
+
+resource "aws_iam_user_policy" "terraform_state_policy" {
+  name = "terraform-state-policy"
+  user = module.iam_user.iam_user_name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:ListBucket",
+          "s3:GetBucketVersioning",
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ]
+        Effect = "Allow"
+        Resource = [
+          module.s3_bucket.s3_bucket_arn,
+          "${module.s3_bucket.s3_bucket_arn}/*"
+        ]
+      },
+      {
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:DeleteItem"
+        ]
+        Effect   = "Allow"
+        Resource = module.dynamodb_table.dynamodb_table_arn
+      }
+    ]
+  })
+}
